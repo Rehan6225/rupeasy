fb5bc6156e729c677ec54272cf3cd98ae8bed3a1
Ashish Agrawal <61875203+ashish-agr@users.noreply.github.com>
2020-10-31 23:41:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hero from "./components/Hero/Hero"
+
+function App() {
+  return (
+    <React.Fragment>
+      <Hero />
+    </React.Fragment>
+  );
+}
+
+export default App;
